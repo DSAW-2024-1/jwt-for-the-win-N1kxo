485709ffc63c18f1920c424ff20f64089b8d92cb
N1kxo <nico9ms@hotmail.com>
2024-05-15 23:39:31 -0500

    BugFix contactsRoutes.js

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -4,7 +4,7 @@ const { getRandomContacts } = require('../controllers/contactsController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 // Endpoint protegido para obtener una lista aleatoria de contactos
-router.get('/', authMiddleware.verifySession, (req, res) => {
+router.get('/contacts', authMiddleware.verifySession, (req, res) => {
     const contacts = getRandomContacts();
     res.json({ contacts });
 });
